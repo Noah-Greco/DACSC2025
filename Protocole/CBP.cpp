aa9a9057d9f11a283d57245af662f80853d3cc58
François-Dewez <fransdewez@gmail.com>
2025-10-12 15:05:30 +0200

    Correction Search et book consultations

--- Protocole/CBP.cpp
+++ Protocole/CBP.cpp
@@ -16,16 +16,22 @@ char CBP_Book_Consultation(char* consultationId, char* reason);
 
 int estPresent(int socket);
 
+int clients[NB_MAX_CLIENTS];
+int nbClients = 0;
+
+pthread_mutex_t mutexClients = PTHREAD_MUTEX_INITIALIZER;
+
 bool CBP(char* requete, char* reponse,int socket)
 {
 	char *ptr = strtok(requete,"#");
 
 	if(strcmp(ptr,"LOGIN") == 0)
 	{
-		char user[50], password[50];
+		char user[50], password[50], NoPatient[10];
 
 		strcpy(user,strtok(NULL,"#"));
 		strcpy(password,strtok(NULL,"#"));
+		strcpy(NoPatient,strtok(NULL,"#"));
 
 		printf("\t[THREAD %p] LOGIN de %s\n",pthread_self(),user);
 
@@ -49,21 +55,16 @@ bool CBP(char* requete, char* reponse,int socket)
 				return false;
 			}
 		}
-
 	}
+
 	if (strcmp(ptr,"LOGOUT") == 0)
 	{
 		printf("\t[THREAD %p] LOGOUT\n",pthread_self());
 		retire(socket);
 		sprintf(reponse,"LOGOUT#ok");
 		return false;
-
 	}
-	//Pas encore eu le temps de les compléter. Il faudrait implémenter la lecture dans la base de données
-	//Soit tu peux faire la lecture dans la BD soit tu peux continuer les serveurs ou clients réservation.
-	//Pour lire dans la bd j'ai mis toute les infos de la bd dans le readme et chat dis qu'on peut faire des lectures dans la bd depuis le code 
-	//donc ce serait ce qu'il faudrait faire. Si tu fais Client ou serveur réservation j'ai pas encore regarder mais si c ce que tu fais fais moi une note
-	//pour m'expliquer ce que tu as fait 
+	
 	if(strcmp(ptr, "GET_SPECIALTIES") == 0)
 	{
 		MYSQL * connection;
@@ -115,8 +116,6 @@ bool CBP(char* requete, char* reponse,int socket)
 
 			}
 		}
-
-
 	}
 
 	if(strcmp(ptr, "GET_DOCTORS") == 0)
@@ -172,14 +171,125 @@ bool CBP(char* requete, char* reponse,int socket)
 		}
 	}
 
-	if(strcmp(ptr, "SEARCH_CONSULTATIONS") == 0)
+	//on envoie le nom du docteur puis on récupère son ID ou on envoie son id directement ?
+	//on doit récupérer date de fin mais à quoi elle sert ?
+	if(strcmp(ptr, "SEARCH_CONSULTATIONS") == 0) 
 	{
-		
+		char * specialties = strtok(NULL, "#");
+		char * doctors = strtok(NULL, "#");
+		char * startDate = strtok(NULL, "#");
+		char * endDate = strtok(NULL, "#");
+
+		MYSQL * connection;
+		connection = mysql_init(NULL);
+
+		if(!connection)
+		{
+			fprintf(stderr, "mysql_init failed\n");
+		}
+
+		else
+		{
+			if(mysql_real_connect(connection, "localhost","Student","PassStudent1_","PourStudent",0,NULL,0) == NULL)
+			{
+				fprintf(stderr, "connect : %s\n", mysql_error(connection));
+			}
+
+			else
+			{
+				char sql_cmd[500];
+
+				sprintf(sql_cmd, "SELECT * FROM consultations c inner join doctors d on (c.doctor_id = d.id) inner join specialties s on 
+					(d.specialty_id = s.id) where c.doctor_id like %s and c.date like %s and s.name like %s;", doctors, dateDeb, specialties);
+
+				if(mysql_query(connection, sql_cmd))
+				{
+					fprintf(stderr, "Query : %s\n", mysql_error(connection));
+					mysql_close(connection);
+				}
+
+				else
+				{
+					MYSQL_RES * res = mysql_store_result(connection);
+
+					if(!res)
+					{
+						fprintf(stderr, "store_result : %s\n", mysql_error(connection));
+						mysql_close(connection);
+					}
+
+					else
+					{
+						MYSQL_ROW row;
+
+						while((row = mysql_fetch_row(res)) != NULL)
+						{
+							printf("Specialité : %s\n", row[0]);
+						}
+
+						mysql_free_result(res);
+						mysql_close(connection);
+					}
+				}
+
+			}
+		}
 	}
 
 	if(strcmp(ptr, "BOOK_CONSULTATION") == 0)
 	{
-		
+		char * cons_id = strtok(NULL, "#");
+		char * reason = strtok(NULL, "#");
+
+		MYSQL * connection;
+		connection = mysql_init(NULL);
+
+		if(!connection)
+		{
+			fprintf(stderr, "mysql_init failed\n");
+		}
+
+		else
+		{
+			if(mysql_real_connect(connection, "localhost","Student","PassStudent1_","PourStudent",0,NULL,0) == NULL)
+			{
+				fprintf(stderr, "connect : %s\n", mysql_error(connection));
+			}
+
+			else
+			{
+				if(mysql_query(connection, ""))
+				{
+					fprintf(stderr, "Query : %s\n", mysql_error(connection));
+					mysql_close(connection);
+				}
+
+				else
+				{
+					MYSQL_RES * res = mysql_store_result(connection);
+
+					if(!res)
+					{
+						fprintf(stderr, "store_result : %s\n", mysql_error(connection));
+						mysql_close(connection);
+					}
+
+					else
+					{
+						MYSQL_ROW row;
+
+						while((row = mysql_fetch_row(res)) != NULL)
+						{
+							printf("Specialité : %s\n", row[0]);
+						}
+
+						mysql_free_result(res);
+						mysql_close(connection);
+					}
+				}
+
+			}
+		}
 	}
 }
 
