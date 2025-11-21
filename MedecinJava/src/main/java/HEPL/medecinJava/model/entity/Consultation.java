f4069663cace0aca603b85b7ea6a3328dec107a7
Francois-Dewez <fransdewez@gmail.com>
2025-11-21 10:53:46 +0100

    Avancement sur les classe entities

---
+++ MedecinJava/src/main/java/HEPL/medecinJava/model/entity/Consultation.java
@@ -0,0 +1,17 @@
+package HEPL.medecinJava.model.entity;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Consultation {
+    private int id;
+    private int doctor_id;
+    private int patient_id;
+    private LocalDate dateConsultation;
+    private LocalTime timeConsultation;
+    private String reason;
+
+    
+
+
+}
